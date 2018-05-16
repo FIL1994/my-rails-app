d0ae5ef6ebbf8d697a775a007bd5a259e0dba14c
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-05-15 21:41:21 -0400

    configured custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  # get 'pages/contact'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
