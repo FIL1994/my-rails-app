0eacd71de7369d66e7e5b0dd5a8327d34d08af07
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-05-16 21:30:41 -0400

    integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,20 +1,22 @@
 class Portfolio < ApplicationRecord
-    has_many :technologies
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: ->(attrs) { attrs['name'].blank? }
 
-    def self.angular
-        where(subtitle: 'Angular')
-    end
+  include Placeholder
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
-    # same as self.angular
-    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } 
+  def self.angular
+    where(subtitle: 'Angular')
+  end
 
-    after_initialize :set_defaults
+  # same as self.angular
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
-    def set_defaults
-        # if nil set default 
-        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+  after_initialize :set_defaults
+
+  def set_defaults
+    # if nil set default
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
